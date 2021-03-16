66bee37ecd2a06d5617da8070dcf5209ad1aa094
Gauthier Donikian <gauthier.donikian@gmail.com>
2021-03-16 22:01:06 +0100

    commit added

--- commit.sh
+++ commit.sh
@@ -1,3 +1,2 @@
-KEY=`curl -s -kd 'user=admin&password=$2&type=keygen' https://$1/api | awk '{print $NF $(NF-1)}' | awk -F '>' '{print $(NF-3)}' | awk -F '<' '{print $(NF-1)}'`
-echo $KEY
+KEY=`curl -s -kd 'user=admin&password='$2'&type=keygen' https://$1/api | awk '{print $NF $(NF-1)}' | awk -F '>' '{print $(NF-3)}' | awk -F '<' '{print $(NF-1)}'`
 curl -kd "type=commit&cmd=<commit><description>This is my commit message</description></commit>&key=$KEY" https://$1/api
