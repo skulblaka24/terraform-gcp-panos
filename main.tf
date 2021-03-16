687292b7866defa44f74748bb9666d38192e76cf
Gauthier Donikian <gauthier.donikian@gmail.com>
2021-03-16 11:55:45 +0100

    Fix

--- main.tf
+++ main.tf
@@ -39,6 +39,6 @@ resource "null_resource" "commit_fw" {
 #  }
 
   provisioner "local-exec" {
-    command = "./commit.sh var.fw_ip var.password"
+    command = "./commit.sh ${fw_ip} ${password}"
   }
 }
